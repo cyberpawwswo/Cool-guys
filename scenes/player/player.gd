91fba4f1a475a0daad3bb9cd95715cbb84bd5a0a
cyberpawwswo <zar.0407119@gmail.com>
2026-08-14 12:12:21 +0300

    создало игрока

---
+++ scenes/player/player.gd
@@ -0,0 +1,41 @@
+extends CharacterBody3D
+
+const WALK_SPEED = 5.0
+const SPRINT_SPEED = 9.0
+const JUMP_VELOCITY = 4.5
+const MOUSE_SENSITIVITY = 0.005
+
+@export var camera: Camera3D
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
+		camera.rotate_x(-event.relative.y * MOUSE_SENSITIVITY)
+		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	var speed := SPRINT_SPEED if Input.is_action_pressed("sprint") else WALK_SPEED
+	
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+
+	move_and_slide()
