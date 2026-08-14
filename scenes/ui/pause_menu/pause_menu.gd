ba06ed88bfc86a28c498e3fd438d865d2f305bd4
cyberpawwswo <zar.0407119@gmail.com>
2026-08-14 12:01:59 +0300

    сделал меню паузы делаю игрока

--- scenes/ui/pause_menu/pause_menu.gd
+++ scenes/ui/pause_menu/pause_menu.gd
@@ -1,6 +1,6 @@
 extends Control
+@onready var exit_the_menu := load("res://scenes/ui/main_menu/main_menu.tscn")
 
-@onready var game_scene := preload("res://scenes/ui/pause_menu/PauseMenu.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,9 +11,9 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_packed(game_scene)
+func _on_resume_button_pressed() -> void:
+	pass # Replace with function body.
 
 
-func _on_quit_button_pressed() -> void:
-	get_tree().quit()
+func _on_exit_the_menu_button_pressed() -> void:
+	get_tree().change_scene_to_packed(exit_the_menu)
